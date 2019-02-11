7e0a20c890ddac51ba871ca860eeeafd24df39e7
Dasha Day Hisoler <dhisoler@hawk.iit.edu>
2019-02-10 20:35:27 -0600

    Import other blocks of code from Stolley's bonus demo repo

--- site.js
+++ site.js
@@ -8,34 +8,65 @@
     return;
   }
 
+  function eq(value,condition) {
+    return value === condition;
+  }
+
+  function clean_whitespace(value) {
+    // returns value with all whitespace characters removed
+    return value.replace(/\s/g, '');
+  }
+
   // Email validity function
  function validate_email(value) {
    var email = clean_whitespace(value);
    return validate(email,/^[^@\s]+@[^@\s]+$/g);
  }
 
-
- document.addEventListener('DOMContentLoaded',function(){
-   // Select the necessary elements from the DOM
-   var signup_form = document.querySelector('#emergencyalerts')
-   var signup_submit = document.querySelector('#signup');
-   var email_input = document.querySelector('#email');
-   var tel_input = document.querySelector('#telephone');
-   // Disable the submit button until we are reasonable sure
-   // that we have a ten-digit phone number
-   signup_submit.setAttribute('disabled','disabled');
-
-   // Listen for keyup event ANYWHERE in the form
-   signup_form.addEventListener('keyup',function(){
-     // Check the likely validity of phone AND email
-     if (validate_us_phone(tel_input.value) && validate_email(email_input.value)) {
-       // If both are valid, remove the disabled attribute on the submit button
-       signup_submit.removeAttribute('disabled');
+ // All purpose validate function. It takes a value,
+   // along with either a regular expression pattern or
+   // a simple function -- like the comparison functions
+   // above -- and a condition. JavaScript doesn't char
+   // if a function is called with more or fewer arguments
+   // than described in the function definition, so it's
+   // no problem at all to leave off the `condition`
+   // argument when calling a check that's a regular expression
+   function validate(value,check,condition) {
+     if (eq(typeof(check.test),'function')) {
+       // Handle a regular expression
+       return check.test(value);
+     } else if (eq(typeof(check),'function')) {
+       // Handle a comparison function
+       return check(value,condition);
      } else {
-       // This will re-disable the submit button if the input changes to an invalid state
-       signup_submit.setAttribute('disabled','disabled');
+       return false;
      }
-   });
+   }
 
- // End of DOMContentLoaded
- });
+   document.addEventListener('DOMContentLoaded',function(){
+      // Select the necessary elements from the DOM
+      var signup_form = document.querySelector('#emergencyalerts')
+      var signup_submit = document.querySelector('#signup');
+      var email_input = document.querySelector('#email');
+      var tel_input = document.querySelector('#telephone');
+      // Disable the submit button until we are reasonable sure
+      // that we have a ten-digit phone number
+      signup_submit.setAttribute('disabled','disabled');
+
+      // Listen for keyup event ANYWHERE in the form
+      signup_form.addEventListener('keyup',function(){
+        // Check the likely validity of phone AND email
+        if (validate_us_phone(tel_input.value) && validate_email(email_input.value)) {
+          // If both are valid, remove the disabled attribute on the submit button
+          signup_submit.removeAttribute('disabled');
+        } else {
+          // This will re-disable the submit button if the input changes to an invalid state
+          signup_submit.setAttribute('disabled','disabled');
+        }
+      });
+
+    // End of DOMContentLoaded
+    });
+
+ // End of IIFE
+ }());
