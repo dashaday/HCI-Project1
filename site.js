424639727e722a1b740a1d03680ec08c9ea246cd
Dasha Day Hisoler <dhisoler@hawk.iit.edu>
2019-02-10 20:38:15 -0600

    Modify assigned IDs of variables

--- site.js
+++ site.js
@@ -44,26 +44,24 @@
    }
 
    document.addEventListener('DOMContentLoaded',function(){
-      // Select the necessary elements from the DOM
-      var signup_form = document.querySelector('#emergencyalerts')
-      var signup_submit = document.querySelector('#signup');
-      var email_input = document.querySelector('#email');
-      var tel_input = document.querySelector('#telephone');
-      // Disable the submit button until we are reasonable sure
-      // that we have a ten-digit phone number
-      signup_submit.setAttribute('disabled','disabled');
+     // Select the necessary elements from the DOM
+     var signup_submit = document.querySelector('#submit');
+     var email_input = document.querySelector('#email');
+     // Disable the submit button until we are reasonable sure
+     // that we have a ten-digit phone number
+     signup_submit.setAttribute('disabled','disabled');
 
-      // Listen for keyup event ANYWHERE in the form
-      signup_form.addEventListener('keyup',function(){
-        // Check the likely validity of phone AND email
-        if (validate_us_phone(tel_input.value) && validate_email(email_input.value)) {
-          // If both are valid, remove the disabled attribute on the submit button
-          signup_submit.removeAttribute('disabled');
-        } else {
-          // This will re-disable the submit button if the input changes to an invalid state
-          signup_submit.setAttribute('disabled','disabled');
-        }
-      });
+     // Listen for keyup event ANYWHERE in the form
+     email_input.addEventListener('keyup',function(){
+       // Check the likely validity of phone AND email
+       if (validate_email(email_input.value)) {
+         // If both are valid, remove the disabled attribute on the submit button
+         signup_submit.removeAttribute('disabled');
+       } else {
+         // This will re-disable the submit button if the input changes to an invalid state
+         signup_submit.setAttribute('disabled','disabled');
+       }
+     });
 
     // End of DOMContentLoaded
     });
